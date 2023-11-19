d5d615760b4c5595a634aff08875a248f8b792cc
vishnukkrishna <vishnukkrishna1999@gmail.com>
2023-11-19 21:04:41 +0530

    App.jsx bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import SignUp from "./components/Register/SignUp";
+import Footer from "./components/Header/Accounts/Footer";
+import Banner from "./components/Candidate/Banner";
+import HomePage from "./pages/Candidate/HomePage";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<Login />} /> */}
+          {/* <Route path="dash/" element={<EmployerDashboard />} /> */}
+
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
