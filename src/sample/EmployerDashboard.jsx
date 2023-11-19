418a81bf10d6416623e095cf948d1bd7dcf3c675
adish-ct <adishathu525@gmail.com>
2023-11-19 21:09:22 +0530

    data added

--- src/sample/EmployerDashboard.jsx
+++ src/sample/EmployerDashboard.jsx
@@ -1,37 +1,15 @@
 import React from 'react'
-import { MdDashboard } from "react-icons/md";
-import { FaSuitcase, FaRegCalendarTimes, FaUserFriends } from "react-icons/fa";
-import { IoSettings } from "react-icons/io5";
+
 import { Typography, Card, CardBody, Button } from '@material-tailwind/react';
+import Sidebar from '../myComponents/employer/Sidebar';
+import DashboardSmallCard from '../myComponents/DashboardSmallCard';
 
 function EmployerDashboard() {
     var ar = ['a', 'b', 'c', 'd']
     return (
         <>
             <div className="flex">
-                {/* side bar */}
-                <div className="h-screen text-center bg-black w-1/6">
-                    <div className="flex flex-col items-center gap-5">
-                        <div className="text-start flex flex-col gap-7 text-white">
-                            <div className="flex flex-row gap-3">
-                                <MdDashboard /><h1>Dashboard</h1>
-                            </div>
-                            <div className="flex flex-row gap-3">
-                                <FaSuitcase /><h1>Jobs</h1>
-                            </div>
-                            <div className="flex flex-row gap-3">
-                                <FaUserFriends /><h1>Candidates</h1>
-                            </div>
-                            <div className="flex flex-row gap-3">
-                                <FaRegCalendarTimes /><h1>Interviews</h1>
-                            </div>
-                            <div className="flex flex-row gap-3">
-                                <IoSettings /><h1>Settings</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* side bar end */}
+                <Sidebar />
                 {/* main dashboard */}
                 <div className="h-screen bg-deep-orange-100 w-5/6">
                     {/* Dashboard Heading */}
@@ -46,19 +24,7 @@ function EmployerDashboard() {
                     <div className="flex flex-row justify-around">
                         {
                             ar.map(() => (
-                                <Card className="mt-6 w-80">
-                                    <CardBody className='text-center'>
-                                        <Typography variant="h5" color="blue-gray" className="mb-2">
-                                            Followers
-                                        </Typography>
-                                        <Typography>
-                                            count
-                                        </Typography>
-                                        <Typography>
-                                            Additional
-                                        </Typography>
-                                    </CardBody>
-                                </Card>
+                                <DashboardSmallCard />
                             ))
                         }
                     </div>
