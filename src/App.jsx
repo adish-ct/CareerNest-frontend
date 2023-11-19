faf9892f003d085d5eb19dc64ab2fb911e7ad09c
adish-ct <adishathu525@gmail.com>
2023-11-19 21:11:21 +0530

    app modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import './App.css'
-
+import { useDispatch, useSelector } from 'react-redux';
+import Login from './sample/Login';
+import { BrowserRouter as Router, Route, Routes, Navigate, } from "react-router-dom";
+import EmployerDashboard from './sample/EmployerDashboard';
+import CreateJob from './sample/CreateJob';
 
 const App = () => {
+  const value = useSelector((state) => {
+    return state.value;
+  })
 
+  const loading = useSelector((state) => {
+    return state.loading;
+  })
+  const dispatch = useDispatch()
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="dash/" element={<EmployerDashboard />} />
+          <Route path="create-job/" element={<CreateJob />} />
+
+          <Route />
+        </Routes>
+      </Router>
     </>
   )
 }
