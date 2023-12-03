c30094ef85b433e73c1100a864bceecb058d09ee
adish-ct <adishathu525@gmail.com>
2023-12-03 17:52:02 +0530

    basic user information updation completed

--- src/components/Candidate/Profile/ProfileBasicInformationForm.jsx
+++ src/components/Candidate/Profile/ProfileBasicInformationForm.jsx
@@ -5,8 +5,13 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { LiaUserEditSolid } from 'react-icons/lia';
 import { Button } from '@material-tailwind/react';
+import axios from 'axios';
+import { baseUrl } from '../../../api/Api';
+import { useSelector } from 'react-redux';
 
 function ProfileBasicInformationForm({ userDetails }) {
+
+    const user = useSelector((state) => state.user)
     const [userToggle, setUserToggle] = useState(false);
 
     const formik = useFormik({
@@ -22,18 +27,16 @@ function ProfileBasicInformationForm({ userDetails }) {
             phone: Yup.string().matches(/^[0-9]{10}$/, 'Invalid phone number').required("* required field")
         }),
         onSubmit: async (values, { setSubmitting }) => {
-            console.log("working");
-            // Simulate an async API call (remove this in your actual implementation)
-            setTimeout(() => {
-                console.log("form submit", values);
+            if (user) {
+                const response = await axios.put(`${baseUrl}/accounts/get-user/${user.user_id}/`, values)
                 toast.success('Form submitted successfully!');
-                setSubmitting(false);
-            }, 500);
+                setUserToggle(!userToggle)
+            }
+
         }
     });
 
     useEffect(() => {
-        console.log(formik.errors);
     }, [formik.errors]);
 
     return (
@@ -86,10 +89,18 @@ function ProfileBasicInformationForm({ userDetails }) {
                             </label>
                             <input
                                 type="text"
-                                className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                name='phone'
                                 value={formik.values.phone}
-                                onChange={(e) => handleInputChange('phone', e.target.value)}
+                                onChange={formik.handleChange}
+                                className={
+                                    formik.errors.phone && formik.touched.phone
+                                        ? 'form-control shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-red-500'
+                                        : 'form-control shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                                }
                             />
+                            {formik.errors.phone && formik.touched.phone && (
+                                <small className="text-red-500 text-xs italic">{formik.errors.phone}</small>
+                            )}
                         </div>
                     </div>
                     <div className="w-full lg:w-6/12 px-4">
@@ -102,10 +113,18 @@ function ProfileBasicInformationForm({ userDetails }) {
                             </label>
                             <input
                                 type="text"
-                                className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                name='first_name'
                                 value={formik.values.first_name}
-                                onChange={(e) => handleInputChange('first_name', e.target.value)}
+                                onChange={formik.handleChange}
+                                className={
+                                    formik.errors.first_name && formik.touched.first_name
+                                        ? 'form-control shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-red-500'
+                                        : 'form-control shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                                }
                             />
+                            {formik.errors.first_name && formik.touched.first_name && (
+                                <small className="text-red-500 text-xs italic">{formik.errors.first_name}</small>
+                            )}
                         </div>
                     </div>
                     <div className="w-full lg:w-6/12 px-4">
@@ -118,9 +137,10 @@ function ProfileBasicInformationForm({ userDetails }) {
                             </label>
                             <input
                                 type="text"
+                                name='last_name'
                                 className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                                 value={formik.values.last_name}
-                                onChange={(e) => handleInputChange('last_name', e.target.value)}
+                                onChange={formik.handleChange}
                             />
                         </div>
                     </div>
